042651af4cd8563fec1c24f05563909064d0956c
Agung Wicaksana <agung.wicaksaana@gmail.com>
2021-07-06 09:20:06 +0700

    Topping -cms

---
+++ app/Http/Controllers/ToppingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topping;
+use Illuminate\Http\Request;
+
+class ToppingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Topping  $topping
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Topping $topping)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Topping  $topping
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Topping $topping)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Topping  $topping
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Topping $topping)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Topping  $topping
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Topping $topping)
+    {
+        //
+    }
+}
--- database/migrations/2021_07_06_091725_create_toppings_table.php
+++ database/migrations/2021_07_06_091725_create_toppings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePacketsTable extends Migration
+class CreateToppingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreatePacketsTable extends Migration
      */
     public function up()
     {
-        Schema::create('packets', function (Blueprint $table) {
+        Schema::create('toppings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('packet_category_id');
-            $table->foreign('packet_category_id')->references('id')->on('packet_categories')->cascadeOnDelete();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->integer('quota'); // MB
             $table->integer('price');
-            $table->integer('point_reward');
+            $table->integer('quota'); // MB
             $table->integer('active_period'); // day
             $table->timestamps();
         });
@@ -34,6 +31,6 @@ class CreatePacketsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('packets');
+        Schema::dropIfExists('toppings');
     }
 }
